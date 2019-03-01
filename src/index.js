225d2ddecc35bc4ac558d39978f90e2d805aa6ee
Jeff Ross <jross323@gmail.com>
2019-03-01 14:56:23 -0500

    extra challenge #2

--- src/index.js
+++ src/index.js
@@ -97,7 +97,12 @@ class Game extends React.Component {
                 'Go to game start';
             return (
                 <li key={move}>
-                    <button onClick={() => this.jumpTo(move)}>{desc}</button>
+                    <button 
+                        onClick={() => this.jumpTo(move)}
+                        className={(move === this.state.stepNumber) ? 'current-move' : ''}
+                    >
+                        {desc}
+                    </button>
                 </li>
             );
         });
